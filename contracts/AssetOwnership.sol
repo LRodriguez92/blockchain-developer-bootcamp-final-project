9f3652b496c28b3e4fcecc1e7c4d2f303307e9d1
Leonardo <leonardo.rodriguez92@gmail.com>
2021-11-13 14:11:58 -0500

    added modifier logic

--- contracts/AssetOwnership.sol
+++ contracts/AssetOwnership.sol
@@ -1,46 +1,79 @@
-mapping (uint => Asset) assets;
+pragma solidity ^0.8.0;
 
-enum State{InPossesion, LostOrStolen, Destroyed}
+contract AssetOwnership {
 
-struct Asset {
-    string name;
-    uint serial;
-    address owner;
-    State state;
-}
+    // owner
+    address public owner;
 
-modifier verifyCaller (address _address) {
+    // assets
+    mapping (uint => Asset) assets;
+
+    // asset state
+    enum State{InPossesion, LostOrStolen, Destroyed, TransferredOwnership}
+
+    // asset struct
+    struct Asset {
+        string name;
+        uint serial;
+        address owner;
+        State state;
+    }
 
     // verifies the caller is the owner of the asset
+    modifier verifyCaller (uint _serial) {
+        require(msg.sender == assets[_serial].owner, "You are not the owner of this asset");
+        _;
+    }
 
-}
+    // verifies the asset is in possesion (use when transferring ownership)
+    modifier inPossesion (uint _serial) {
+        require(assets[_serial].state == State.InPossesion, "The asset is not currently in your possesion"); 
+        _;
+    }
 
-function addAsset(string memory _name, uint _serial) {
-    
-    // creates new Asset struct and adds it to asset mapping
+    // verifies the asset is reported lost or stolen ()
+    modifier isLostorStolen (uint _serial) {
+        require(assets[_serial].state == State.LostOrStolen, "This asset has not been reported lost or stolen");
+        _;
+    }
 
-}
+    modifier isNotDestroyed (uint _serial) {
+        require(assets[_serial].state != State.Destroyed, "This asset has been destroyed. No further actions are available");
+        _;
+    }
 
-function reportLostOrStolen(uint _serial) {
+    modifier transferOwnership (uint _serial) {
+        require(assets[_serial].state == State.TransferredOwnership, "Ownership has not been transferred.");
+        _;
+    }
 
-    // changes the state to LostOrStolen
+    function addAsset(string memory _name, uint _serial) {
+        
+        // creates new Asset struct and adds it to asset mapping
 
-}
+    }
 
-function reportFoundAndInPosession(uint _serial) {
+    function reportLostOrStolen(uint _serial) {
 
-    // changes the state to InPossesion
+        // changes the state to LostOrStolen
 
-}
+    }
 
-function transferOwnership(uint _serial) {
+    function reportFoundAndInPosession(uint _serial) {
 
-    // transfers ownership of asset
+        // changes the state to InPossesion
 
-}
+    }
 
-function destroyAsset(uint _serial) {
+    function transferOwnership(uint _serial) {
 
-    // chnages the state to Destroyed
+        // transfers ownership of asset
 
+    }
+
+    function destroyAsset(uint _serial) {
+
+        // chnages the state to Destroyed
+
+    }
 }
