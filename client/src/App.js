f2e13ce521acfd548ab9391ac80bac1f93bdbfbf
Leonardo <leonardo.rodriguez92@gmail.com>
2021-12-04 16:35:37 -0500

    user can create a new asset

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Web3 from 'web3';
 import assetOwnershipAbi from './contracts/AssetOwnership.json'
 import { useWallet, UseWalletProvider } from 'use-wallet' 
 import './App.css';
+
+import NewAsset from './components/NewAsset';
 
 // const developmentContractAddress = '0x415De09609e14878c781349E95E4e8Af1943f3F7';
 
@@ -15,21 +17,39 @@ function App() {
 
   const wallet = useWallet()
 
-  const [walletAddress, setWalletAddress] = useState('0x');
-  
   const [serial, setSerial] = useState(0);
   const [getAsset, setGetAsset] = useState({});
+
+  useEffect(() => {
+    console.log(AssetContract)
+  }, [])
+  
 
   const fetchAsset = async (e) => {
     e.preventDefault();
     console.log("Fetching asset");
 
-    const accounts = await window.ethereum.enable();
-    const account = account[0];
+    // const accounts = await window.ethereum.enable();
+    // const account = account[0];
 
-    const result = await AssetContract.methods.fetchAsset(serial);
+    const result = await AssetContract.methods.fetchAsset(serial).call();
 
     setGetAsset(result);
+  }
+
+  const createAsset = async (asset) => {
+    console.log("Creating asset: ", asset);
+
+    try {
+      const result = await AssetContract.methods.addAsset(asset.name, asset.serial, asset.value).send({
+        from: wallet.account
+      });
+      console.log(result);
+
+      // TODO: Display a response to the user
+    } catch (error) {
+      console.error(error); 
+    }
   }
 
   return (
@@ -39,6 +59,8 @@ function App() {
           <div>Account: {wallet.account}</div>
           <div>Balance: {wallet.balance}</div>
           <button onClick={() => wallet.reset()}>Disconnect MetaMask</button>
+          
+          <NewAsset createAsset={createAsset} />
         </div>
       ) : (
         <div>
---
+++ client/src/components/NewAsset.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const NewAsset = (props) => {
+    
+    const [newAsset, setNewAsset] = useState({
+        name: '',
+        serial: 0,
+        value: 0,
+      });
+
+    const handleChange = (e) => {
+        setNewAsset({
+            ...newAsset,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submitting...");
+        props.createAsset(newAsset);
+    }
+
+    return (
+        <div>
+            <h1>New Asset</h1>
+            <form onSubmit={handleSubmit}>
+                <input name="name" type="text" placeholder="Asset name" onChange={handleChange} />
+                <input name="serial" type="number" placeholder="Serial #" onChange={handleChange} />
+                <input name="value" type="number" placeholder="Value" onChange={handleChange} />
+                <input type="submit" value="Create Asset"/>
+            </form>
+        </div>
+    )
+}
+
+export default NewAsset;
